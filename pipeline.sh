1cca6d91760a34607361bd9ae16e049de32b0332
Abdou Niang <ehaniang@gmail.com>
2017-06-28 01:31:36 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
+#! /bin/bash
 
+curl -L https://bootstrap.saltstack.com -o /tmp/bootstrap_salt.sh
+chmod +x /tmp/bootstrap_salt.sh
+
+systemctl stop salt-minion
+
+salt-call --local pillar.items
+
+salt-call --local  state.apply -l debug
